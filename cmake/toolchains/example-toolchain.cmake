db0694c374ceb0936261177fef5ee58b225f72b8
stroma <stroma@gmx.com>
2026-04-29 08:50:12 +0000

    Added dev container workflow: Windows VSCode → SSH into Ubuntu → Dev Container (Docker) → build + debug → remote target VSCode → Ubuntu (via Remote SSH) Ubuntu → Container (via Dev Containers)

--- cmake/toolchains/example-toolchain.cmake
+++ cmake/toolchains/example-toolchain.cmake
@@ -2,7 +2,8 @@ set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
 # Use FULL paths (recommended)
-set(TOOLCHAIN_ROOT /home/ico/sysroots/aarch64-cortexa53-linux-gnu)
+#set(TOOLCHAIN_ROOT /home/ico/sysroots/aarch64-cortexa53-linux-gnu)
+set(TOOLCHAIN_ROOT /opt/cross/aarch64-cortexa53-linux-gnu)
 set(TRIPLET aarch64-cortexa53-linux-gnu)
 # Compilers
 set(CMAKE_C_COMPILER   ${TOOLCHAIN_ROOT}/bin/${TRIPLET}-gcc)
@@ -10,7 +11,7 @@ set(CMAKE_CXX_COMPILER ${TOOLCHAIN_ROOT}/bin/${TRIPLET}-g++)
 #set(CMAKE_C_COMPILER /usr/bin/aarch64-linux-gnu-gcc)
 #set(CMAKE_CXX_COMPILER /usr/bin/aarch64-linux-gnu-g++)
 
-# DO NOT set CMAKE_SYSROOT (let GCC handle it)
+# DO NOT set CMAKE_SYSROOT (let GCC handle it) checked with aarch64-cortexa53-linux-gnu-gcc -print-sysroot
 # Optional sysroot (only if you actually have one)
 # if not set use default from /usr/aarch64-linux-gnu on Debian/Ubuntu
 # set(CMAKE_SYSROOT /opt/sysroots/aarch64)
